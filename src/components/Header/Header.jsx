d9a693f9680476f405f2019ee3f65f663f170c85
Himanshu Jangir <hjuberoi16@gmail.com>
2024-03-12 21:38:33 +0530

    latest commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -37,11 +37,19 @@ const Header = () => {
         <div className="navigation">
           <ul className="menu flex item-center gap-[2.7rem]">
             {
-              navLinks.map((link, index) => <li key={index}>
-                <NavLink to={link.path} className={navClass => navClass.isActive 
-                ?'text-primaryColor text-[16px] leading-7 font-[600'
-                :'text-textColor text-[16px] leading-7 font-[600'} >{link.display}</NavLink>
-              </li>)
+              navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink 
+                    to={link.path} 
+                    className={navClass => 
+                      navClass.isActive 
+                        ?'text-primaryColor text-[16px] leading-7 font-[600'
+                        :'text-textColor text-[16px] leading-7 font-[600'} 
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+              ))
             }
           </ul>
         </div>
