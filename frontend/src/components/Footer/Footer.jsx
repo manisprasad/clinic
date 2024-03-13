0a2dc6bd174dd49ecb780f9bc1886952cf03589a
Manish Prasad <100564494+manisprasad@users.noreply.github.com>
2024-03-13 23:50:50 +0530

    Add files via upload

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from "../../assets/images/logo.png";
+import {RiLinkedinFill} from 'react-icons/ri';
+import { AiFillYoutube, AiFillGithub, AiOutlineInstagram } from 'react-icons/ai';
+
+const socialLinks = [
+  {
+    path: 'https://www.linkedin.com/',
+    icon: <RiLinkedinFill className='group-hover:text-white w-4 h-5' />,
+  },
+  {
+    path: 'https://www.youtube.com/',
+    icon: <AiFillYoutube className='group-hover:text-white w-4 h-5' />,
+  },
+  {
+    path: 'https://www.github.com/',
+    icon: <AiFillGithub className='group-hover:text-white w-4 h-5' />,
+  },
+  {
+    path: 'https://www.instagram.com/',
+    icon: <AiOutlineInstagram className='group-hover:text-white w-4 h-5' />,
+  },
+]
+
+
+const quickLinks = [
+  {
+    path: '/',
+    title: 'Home',
+  },
+  {
+    path: '/about',
+    title: 'About us',
+  },
+  {
+    path: '/services',
+    title: 'Services',
+  },
+  {
+    path: '/doctors',
+    title: 'Doctors',
+  },
+  {
+    path: '/location',
+    title: 'Location',
+  },
+  {
+    path: '/appointment',
+    title: 'Book Appointment',
+  },
+]
+
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+  const year = new Date().getFullYear();
+  return <footer className='pb-16 pt-10'>
+    <div className="container">
+      <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
+        <div>
+          <img src={logo} alt="" />
+          <p className='text-[16px] leading-7 font-[400] text-textColor' >copyright {year} </p>
+          <div className='w-full flex gap-4'>
+            {
+              socialLinks.map((link, index) => <Link>
+                <a href={link.path} key={index} className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group  hover:bg-primaryColor hover:border-none'>
+                  {link.icon}
+                </a>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 }
 
 export default Footer
